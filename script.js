8b5c976a2d5526e7a4539c03e3b4fa6b9af994c5
rbhall303 <melon033@hotmail.com>
2021-08-17 11:55:38 +0100

    basic functionality added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+const setups = ['You can do', 'You can be'];
+const pronouns = ['anything', 'everyhing', 'whatever', 'all'];
+const conclusions = ['you want', 'you set your mind to', 'your meant to', 'in the world'];
+
+const getRand = (arr) => {
+    const num = Math.floor(Math.random() * arr.length);
+    return arr[num];
+}
+
+const generateMessage = () => {
+    const setup = getRand(setups);
+    const pronoun  = getRand(pronouns);
+    const conclusion = getRand(conclusions);
+    return setup + ' ' + pronoun + ' ' + conclusion + '!';
+}
+
+console.log(generateMessage());
